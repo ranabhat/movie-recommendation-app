8f76d123029143ce0b3646444d4154e5bd848f5d
Paribesh <paribessh_sab@hotmail.com>
2022-03-17 12:36:27 +0200

    in memory storage implemented that  provides only the endpoints that the application needswith an interface which is tailored to the specific business problems the application implements

---
+++ movie/repository/memrepo.py
@@ -0,0 +1,12 @@
+from movie.domain.movie import Movie
+
+class MemRepo:
+    def __init__(self, data, genre=None):
+        self.data = data
+        self.genre = genre
+
+    def list(self):
+        return [Movie.from_dict(i) for i in self.data]
+
+    def recommended_list(self):
+        return [Movie.from_dict(i) for i in self.data if self.genre in i["genre"]]
