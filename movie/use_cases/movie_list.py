894a1402a971022ce0823a71fbf6628608fe84cf
Paribesh <paribessh_sab@hotmail.com>
2022-03-22 20:59:12 +0200

    take the use cases to Class

--- movie/use_cases/movie_list.py
+++ movie/use_cases/movie_list.py
@@ -1,3 +1,15 @@
-def movie_list_use_case(repo): 
-    return repo.list()
+from movie.repository.abstractrepo import MovieRecommendationRepo
+
+class MovieRecommendationUsecase:
+    def __init__(self, repo_object:MovieRecommendationRepo):
+        self.repo_object = repo_object
+    
+    def movie_list_use_case(self):
+        return self.repo_object.list()
+
+    def movie_recommended_use_case(self):
+        return self.repo_object.recommended_list()
+
+# def movie_list_use_case(repo): 
+#     return repo.list()
 
